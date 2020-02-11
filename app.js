44a6ccc43fbc6a22d49f7e0e596bbfb7b170768b
Chi Vong <chi.vongvien@gmail.com>
2020-02-11 12:44:23 +0100

    Adjusted for heroku

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.BASE_PORT || 5000;
+const HOST = process.env.BASE_HOST || "0.0.0.0";
 
 const connectDB = async() => {
   const DATABASE_URL = require('./config').DATABASE_URL;
--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV !== "production"){
 DATABASE_URL = "mongodb+srv://" + process.env.DB_USERNAME + ":" +
  encodeURIComponent(process.env.DB_PASSWORD) + "@" + process.env.DB_CLUSTER + ".mongodb.net/urlxs?retryWrites=true&w=majority";
 
-BASE_URL = process.env.HOST + ":" + process.env.PORT;
+BASE_URL = process.env.BASE_HOST;
 
 module.exports = {
   DATABASE_URL,
